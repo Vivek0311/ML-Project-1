bb4c21998d39f71a64bec98bc547b220e4f924d0
Vivek0311 <viveksirwal@gmail.com>
2024-09-08 10:02:32 +0530

    Added

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
+# If you have only variable types in my class use dataclass else go ahead with constructors
+# dc are used to create class variables in short
+# here this decorator helps to define class variables without init
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join("artifacts", "test.csv")
+    test_data_path: str = os.path.join("artifacts", "train.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion component")
+        try:
+            df = pd.read_csv("notebook/data/stud.csv")
+            logging.info("Read the dataset as dataframe")
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
+            )
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, random_state=42, test_size=0.2)
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True
+            )
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True
+            )
+
+            logging.info("Ingestion of the data is completed")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
