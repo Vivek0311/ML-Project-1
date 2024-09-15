7d26199d328a6f374968ece313aac329adb97778
Vivek0311 <viveksirwal@gmail.com>
2024-09-15 18:19:16 +0530

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -76,7 +76,8 @@ class ModelTrainer:
                     "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
                 "Linear Regression": {},
-                "XGBRegressor": {
+                "K-Neighbors": {"n_neighbors": [5, 7, 9, 11]},
+                "XGBoost": {
                     "learning_rate": [0.1, 0.01, 0.05, 0.001],
                     "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
@@ -92,7 +93,9 @@ class ModelTrainer:
                 },
             }
 
-            model_report = evaluate_models(X_train, y_train, X_test, y_test, models)
+            model_report = evaluate_models(
+                X_train, y_train, X_test, y_test, models, params
+            )
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
